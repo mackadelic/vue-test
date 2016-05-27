2ba7c4cb9e901c53514a2b0faee21c15772eb3cc
Pedro Silva - TIMWE <pedro.silva@TIMWE.COM>
2016-05-27 15:09:03 +0100

    added server

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var http = require('http');
 /**
  * Create HTTP server.
  */
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8082
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 var server = http.createServer(app);
